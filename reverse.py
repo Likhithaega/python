41f0037048288096f9b8a7f35145148238bb8116
Likhithaega <91673676+Likhithaega@users.noreply.github.com>
2025-06-30 22:10:22 +0530

    Add files via upload
    reverse of a string

---
+++ reverse.py
@@ -0,0 +1,10 @@
+# def reverse(text):
+#     new_text=""
+#     for char in text:
+#         new_text=char+new_text
+#     print(new_text)
+# text=input("Enter the string:")
+# reverse(text)
+text=input("Enter the string:")
+reversed_text=text[::-1]
+print(reversed_text)
